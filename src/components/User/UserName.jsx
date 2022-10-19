aca40a41af129a5dd125a029abb17b5f9bf062d5
MISAEL GUILHARDES DE FREITAS <mgfreitas@brasilseg.com.br>
2022-10-19 14:06:52 -0300

    Adicionando possibilidade de edição do componente de username

--- src/components/User/UserName.jsx
+++ src/components/User/UserName.jsx
@@ -1,4 +1,6 @@
 import styles from './styles.module.scss';
-export const UserName = ({ children }) => (
-  <h1 className={styles.userName}>@{children}</h1>
+export const UserName = ({ children, ...props }) => (
+  <h1 className={styles.userName} {...props}>
+    @{children}
+  </h1>
 );
